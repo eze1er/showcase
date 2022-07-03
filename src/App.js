e4414fa9b3aceed769d0c735ea7d5c9de6ac114c
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-07-02 22:50:55 -0400

    section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import assets from "./assets";
+import styles from "./styles/Global";
 import { Download, Features, SectionWrapper, reverse } from "./components";
 
 const App = () => {
@@ -17,6 +18,24 @@ const App = () => {
         mockupImg={assets.homeCards}
         reverse
       />
+      <Features />
+      <SectionWrapper
+        title="Deployment"
+        description="ProNef is built using Expo which runs natively on all user's devices. You can easily get your app into people's hands"
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+      <div className="px-4 py-2 justify-content items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made with Love by {" "} 
+        <span className="bold">Ezechiel K. Itimbien</span></p>
+      </div>
     </>
   );
 };
