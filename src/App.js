35ba07f28df4fb53207683e4a93d2f42964731f5
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-07-02 21:10:52 -0400

    section wrapper

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import assets from "./assets";
+import { Download, Features, SectionWrapper, reverse } from "./components";
+
+const App = () => {
+  return (
+    <>
+      <SectionWrapper
+        title="You own store of Nifty NFTs. Start Selling & Growing"
+        description="Buy, store, collect NFTs, exchange & earn crypto. Join 25+ million people using ProNef Marketplace"
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+    </>
+  );
+};
+
+export default App;
